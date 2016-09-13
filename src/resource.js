bb9afd0ad260780d61ff59302513073d6010bf5d
anirori <hosodasyun789@gmail.com>
2016-09-13 13:54:13 +0900

    背景、プレイヤー、虫追加

---
+++ src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+  bomb_png: "res/M/bug.png",
+  cart_png: "res/M/cat0.png",
+  strawberry_png: "res/strawberry.png",
+//  leftbutton_png: "res/leftbutton.png",
+//  rightbutton_png: "res/rightbutton.png",
+  background_png: "res/M/game_bg.png",
+//  touchorigin_png: "res/touchorigin.png",
+//  touchend_png: "res/touchend.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
