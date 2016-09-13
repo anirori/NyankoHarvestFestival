fe370028d8de84d6ac46a52bf7bf14a1baffdbcc
anirori <hosodasyun789@gmail.com>
2016-09-13 14:03:31 +0900

    大きさ変更

--- src/resource.js
+++ src/resource.js
@@ -1,10 +1,10 @@
 var res = {
-  bomb_png: "res/M/bug.png",
-  cart_png: "res/M/cat0.png",
+  bomb_png: "res/S/bug.png",
+  cart_png: "res/S/cat0.png",
   strawberry_png: "res/strawberry.png",
 //  leftbutton_png: "res/leftbutton.png",
 //  rightbutton_png: "res/rightbutton.png",
-  background_png: "res/M/game_bg.png",
+  background_png: "res/S/game_bg.png",
 //  touchorigin_png: "res/touchorigin.png",
 //  touchend_png: "res/touchend.png"
 };
